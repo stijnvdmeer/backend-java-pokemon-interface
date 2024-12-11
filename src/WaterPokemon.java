23f9cb6b9afccb238dff5b72d8d9fb71fbf26622
stijnvdmeer <stijn08072002@outlook.com>
2024-12-11 15:42:44 +0100

    Added a damage modifier calculator method to the WaterPokemon class and wrote out the attack methods

--- src/WaterPokemon.java
+++ src/WaterPokemon.java
@@ -14,16 +14,64 @@ public class WaterPokemon extends Pokemon {
         return attacks;
     }
 
+    public double calcDamageModifier(Pokemon gymPokemon) {
+        double damageModifier;
+
+        switch (gymPokemon.getType()) {
+            case "water":
+                damageModifier = 0.5;
+                break;
+            case "grass":
+                damageModifier = 0.75;
+                break;
+            case "electric":
+                damageModifier = 1;
+                break;
+            default:
+                damageModifier = 1.5;
+                break;
+        }
+        return damageModifier;
+    }
+
     public void surf(Pokemon pokemon, Pokemon gymPokemon) {
         System.out.println(pokemon.getName() + " uses surf on " + gymPokemon.getName());
+        int damage = (int) (60 * calcDamageModifier(gymPokemon)); // calculate damage modifier dependend on type
+        System.out.println(gymPokemon.getName() + " loses " + damage + " HP");
+        gymPokemon.setHp(gymPokemon.getHp() - damage); // Change HP
+        System.out.println(gymPokemon.getName() + " has " + gymPokemon.getHp() + " HP remaining");
     }
     public void hydroPump(Pokemon pokemon, Pokemon gymPokemon) {
         System.out.println(pokemon.getName() + " uses hydropump on " + gymPokemon.getName());
+        int damage = (int) (60 * calcDamageModifier(gymPokemon)); // calculate damage modifier dependend on type
+        System.out.println(gymPokemon.getName() + " loses " + damage + " HP");
+        gymPokemon.setHp(gymPokemon.getHp() - damage); // Change HP
+        System.out.println(gymPokemon.getName() + " has " + gymPokemon.getHp() + " HP remaining");
     }
     public void hydroCanon(Pokemon pokemon, Pokemon gymPokemon) {
         System.out.println(pokemon.getName() + " uses hydrocanon on " + gymPokemon.getName());
+        int damage = (int) (60 * calcDamageModifier(gymPokemon)); // calculate damage modifier dependend on type
+        System.out.println(gymPokemon.getName() + " loses " + damage + " HP");
+        gymPokemon.setHp(gymPokemon.getHp() - damage); // Change HP
+        System.out.println(gymPokemon.getName() + " has " + gymPokemon.getHp() + " HP remaining");
     }
     public void rainDance(Pokemon pokemon, Pokemon gymPokemon) {
         System.out.println(pokemon.getName() + " uses raindance on " + gymPokemon.getName());
+        int damage = (int) (30 * calcDamageModifier(gymPokemon));
+        if (gymPokemon.getType() == "electric") {
+            damage = 0;
+            System.out.println("raindance has no effect on the opposing " + gymPokemon.getName());
+        }
+        if (gymPokemon.getType() == "grass") {
+            damage = 30;
+            System.out.println("your raindance has healed the opposing " + gymPokemon.getName() + " by " + damage + " HP");
+            gymPokemon.setHp(gymPokemon.getHp() + damage);
+            System.out.println(gymPokemon.getName() + " has " + gymPokemon.getHp() + " HP remaining");
+        }
+        else {
+            System.out.println(gymPokemon.getName() + " loses " + damage + " HP");
+            gymPokemon.setHp(gymPokemon.getHp() - damage); // Change HP
+            System.out.println(gymPokemon.getName() + " has " + gymPokemon.getHp() + " HP remaining");
+        }
     }
 }
