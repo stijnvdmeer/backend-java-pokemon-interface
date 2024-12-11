bab61c4ae542c5b824263c8c99437c016764df9b
stijnvdmeer <stijn08072002@outlook.com>
2024-12-11 13:30:44 +0100

    Created PokemonGymOwner class deleted variable class

---
+++ src/PokemonGymOwner.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+public class PokemonGymOwner extends PokemonTrainer{
+    public String town;
+
+    public PokemonGymOwner(String name, String town, List<Pokemon> pokemons) {
+        super(name, pokemons);
+        this.town = town;
+    }
+
+    public String getTown() {
+        return town;
+    }
+    public void setTown(String town) {
+        this.town = town;
+    }
+}
