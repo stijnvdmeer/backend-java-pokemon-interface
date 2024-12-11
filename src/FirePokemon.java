4301168ad7e087d3bdd23e722e6a8e42ef903ac3
stijnvdmeer <stijn08072002@outlook.com>
2024-12-11 13:50:00 +0100

    Added a damage modifier calculator method to the FirePokemon class and wrote out the attack methods

--- src/FirePokemon.java
+++ src/FirePokemon.java
@@ -13,17 +13,52 @@ public class FirePokemon extends Pokemon {
     public List<String> getAttacks() {
         return attacks;
     }
+    public double calcDamageModifier(Pokemon gymPokemon) {
+        double damageModifier;
+
+        switch (gymPokemon.getType()) {
+            case "fire":
+                damageModifier = 0.5;
+                break;
+            case "electric":
+                damageModifier = 0.75;
+                break;
+            case "water":
+                damageModifier = 1;
+                break;
+            default:
+                damageModifier = 1.5;
+                break;
+        }
+        return damageModifier;
+    }
 
     public void fireLash(Pokemon pokemon, Pokemon gymPokemon) {
         System.out.println(pokemon.getName() + " uses firelash on " + gymPokemon.getName());
+        int damage = (int) (40 * calcDamageModifier(gymPokemon)); // calculate damage modifier dependend on type
+        System.out.println(gymPokemon.getName() + " loses " + damage + " HP");
+        gymPokemon.setHp(gymPokemon.getHp() - damage); // Change HP
+        System.out.println(gymPokemon.getName() + " has " + gymPokemon.getHp() + " HP remaining");
     }
     public void flameThrower(Pokemon pokemon, Pokemon gymPokemon) {
         System.out.println(pokemon.getName() + " uses flamethrower on " + gymPokemon.getName());
+        int damage = (int) (60 * calcDamageModifier(gymPokemon)); // calculate damage modifier dependend on type
+        System.out.println(gymPokemon.getName() + " loses " + damage + " HP");
+        gymPokemon.setHp(gymPokemon.getHp() - damage); // Change HP
+        System.out.println(gymPokemon.getName() + " has " + gymPokemon.getHp() + " HP remaining");
     }
     public void inferno(Pokemon pokemon, Pokemon gymPokemon) {
         System.out.println(pokemon.getName() + " uses inferno on " + gymPokemon.getName());
+        int damage = (int) (90 * calcDamageModifier(gymPokemon)); // calculate damage modifier dependend on type
+        System.out.println(gymPokemon.getName() + " loses " + damage + " HP");
+        gymPokemon.setHp(gymPokemon.getHp() - damage); // Change HP
+        System.out.println(gymPokemon.getName() + " has " + gymPokemon.getHp() + " HP remaining");
     }
     public void pyroBall(Pokemon pokemon, Pokemon gymPokemon) {
         System.out.println(pokemon.getName() + " uses pyroball on " + gymPokemon.getName());
+        int damage = (int) (80 * calcDamageModifier(gymPokemon)); // calculate damage modifier dependend on type
+        System.out.println(gymPokemon.getName() + " loses " + damage + " HP");
+        gymPokemon.setHp(gymPokemon.getHp() - damage); // Change HP
+        System.out.println(gymPokemon.getName() + " has " + gymPokemon.getHp() + " HP remaining");
     }
 }
