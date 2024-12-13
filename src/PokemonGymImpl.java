78b5214362eb0332c401b7e47807d713f426e96d
stijnvdmeer <stijn08072002@outlook.com>
2024-12-13 13:19:44 +0100

    pokemon faint fix on line 57

--- src/PokemonGymImpl.java
+++ src/PokemonGymImpl.java
@@ -54,8 +54,9 @@ public class PokemonGymImpl implements PokemonGym {
 
             System.out.println("Its " + owner.getName() + "'s turn to attack");
             gymOwnerAttacks(gymPokemon, pokemon);
-            System.out.println("Its " + trainer.getName() + "'s turn to attack");
-            attackOrChange(pokemon, gymPokemon, trainer, owner);
+            if (pokemon.getHp() <= 0) break;                                                                            // If the users pokemon fainted on the opponents attack then,
+            System.out.println("Its " + trainer.getName() + "'s turn to attack");                                       // the users pokemon would be allowed to attack again
+            attackOrChange(pokemon, gymPokemon, trainer, owner);                                                        // I fixed it by adding a check on line-57 and if correct exit the loop
 
         }
         if(pokemon.getHp() <= 0){
